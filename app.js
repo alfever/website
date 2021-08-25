7c79e44b63e37a71afcd4aa59c6a170766835460
Alexander Fournier <alexander.fournier@gmail.com>
2021-08-25 17:32:10 -0400

    modified:   app.js 	modified:   package.json

--- app.js
+++ app.js
@@ -8,14 +8,4 @@ app.get("", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`App up at port ${PORT}`);
-const app = require("express")();
-
-const PORT = process.env.PORT || 3000;
-
-app.get("", (req, res) => {
-  res.send("Hello world");
-});
-
-app.listen(PORT, () => {
-  console.log(`App up at port ${PORT}`);
 });
